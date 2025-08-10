0c97aaac5e70fc5d93f0c0c1dc08bc5a6a0837a5
Yuya Motozono <motozono.tech@gmail.com>
2025-08-10 22:41:07 +0900

    200レスポンスが返ってくるように修正

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v2')->group(function () {
+    require base_path('routes/api/v2/application.php');
 });
