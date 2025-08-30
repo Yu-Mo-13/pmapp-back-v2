484f13d08fc9932bb51a1972c8268bf83766de20
Yuya Motozono <motozono.tech@gmail.com>
2025-08-30 15:27:06 +0900

    アプリケーション作成ユニットテスト

---
+++ tests/Feature/app/Http/Controllers/Application/ApplicationCreateControllerTest.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace Tests\Feature\app\Http\Controllers\Application;
+
+use Tests\PmappTestCase;
+
+class ApplicationCreateControllerTest extends PmappTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->setUpApplication();
+    }
+
+    public function test_アプリケーションが正常に作成できること()
+    {
+        $testApplication = [
+            'application' => [
+                'name' => 'Test Application',
+                'account_class' => true,
+                'notice_class' => true,
+                'mark_class' => true,
+                'pre_password_size' => 8
+            ]
+        ];
+
+        $response = $this->post(route('applications.create'), $testApplication);
+        $response->assertOk();
+        $this->assertDatabaseHas('applications', $testApplication['application']);
+    }
+
+    public function test_アプリケーション名が未入力の場合、バリデーションエラーになること()
+    {
+        $response = $this->post(route('applications.create'), [
+            'application' => [
+                'name' => '',
+                'account_class' => true,
+                'notice_class' => true,
+                'mark_class' => true,
+                'pre_password_size' => 8
+            ]
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['application.name']);
+    }
+
+    public function test_アプリケーション名が文字列でない場合、バリデーションエラーとなること()
+    {
+        $response = $this->post(route('applications.create'), [
+            'application' => [
+                'name' => 123,
+                'account_class' => true,
+                'notice_class' => true,
+                'mark_class' => true,
+                'pre_password_size' => 8
+            ]
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['application.name']);
+    }
+
+    public function test_アプリケーション名が255文字を超える場合、バリデーションエラーとなること()
+    {
+        $longName = str_repeat('a', 256);
+        $response = $this->post(route('applications.create'), [
+            'application' => [
+                'name' => $longName,
+                'account_class' => true,
+                'notice_class' => true,
+                'mark_class' => true,
+                'pre_password_size' => 8
+            ]
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['application.name']);
+    }
+
+    public function test_アプリケーション名が重複する場合、バリデーションエラーとなること()
+    {
+        $existingApplication = $this->accountClassTrueApplication;
+
+        $response = $this->post(route('applications.create'), [
+            'application' => [
+                'name' => $existingApplication->name,
+                'account_class' => true,
+                'notice_class' => true,
+                'mark_class' => true,
+                'pre_password_size' => 8
+            ]
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['application.name']);
+    }
+
+    public function test_アカウント区分が未入力の場合、バリデーションエラーとなること()
+    {
+        $response = $this->post(route('applications.create'), [
+            'application' => [
+                'name' => 'Test Application',
+                'account_class' => null,
+                'notice_class' => true,
+                'mark_class' => true,
+                'pre_password_size' => 8
+            ]
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['application.account_class']);
+    }
+
+    public function test_アカウント区分がboolean型でない場合、バリデーションエラーとなること()
+    {
+        $response = $this->post(route('applications.create'), [
+            'application' => [
+                'name' => 'Test Application',
+                'account_class' => 'yes',
+                'notice_class' => true,
+                'mark_class' => true,
+                'pre_password_size' => 8
+            ]
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['application.account_class']);
+    }
+
+    public function test_定期通知区分が未入力の場合、バリデーションエラーとなること()
+    {
+        $response = $this->post(route('applications.create'), [
+            'application' => [
+                'name' => 'Test Application',
+                'account_class' => true,
+                'notice_class' => null,
+                'mark_class' => true,
+                'pre_password_size' => 8
+            ]
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['application.notice_class']);
+    }
+
+    public function test_定期通知区分がboolean型でない場合、バリデーションエラーとなること()
+    {
+        $response = $this->post(route('applications.create'), [
+            'application' => [
+                'name' => 'Test Application',
+                'account_class' => true,
+                'notice_class' => 'yes',
+                'mark_class' => true,
+                'pre_password_size' => 8
+            ]
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['application.notice_class']);
+    }
+
+    public function test_記号区分が未入力の場合、バリデーションエラーとなること()
+    {
+        $response = $this->post(route('applications.create'), [
+            'application' => [
+                'name' => 'Test Application',
+                'account_class' => true,
+                'notice_class' => true,
+                'mark_class' => null,
+                'pre_password_size' => 8
+            ]
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['application.mark_class']);
+    }
+
+    public function test_記号区分がboolean型でない場合、バリデーションエラーとなること()
+    {
+        $response = $this->post(route('applications.create'), [
+            'application' => [
+                'name' => 'Test Application',
+                'account_class' => true,
+                'notice_class' => true,
+                'mark_class' => 'yes',
+                'pre_password_size' => 8
+            ]
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['application.mark_class']);
+    }
+
+    public function test_仮登録パスワード桁数が未入力の場合、バリデーションエラーとなること()
+    {
+        $response = $this->post(route('applications.create'), [
+            'application' => [
+                'name' => 'Test Application',
+                'account_class' => true,
+                'notice_class' => true,
+                'mark_class' => true,
+                'pre_password_size' => null
+            ]
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['application.pre_password_size']);
+    }
+
+    public function test_仮登録パスワード桁数が整数でない場合、バリデーションエラーとなること()
+    {
+        $response = $this->post(route('applications.create'), [
+            'application' => [
+                'name' => 'Test Application',
+                'account_class' => true,
+                'notice_class' => true,
+                'mark_class' => true,
+                'pre_password_size' => 'eight'
+            ]
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['application.pre_password_size']);
+    }
+
+    public function test_仮登録パスワード桁数が1未満の場合、バリデーションエラーとなること()
+    {
+        $response = $this->post(route('applications.create'), [
+            'application' => [
+                'name' => 'Test Application',
+                'account_class' => true,
+                'notice_class' => true,
+                'mark_class' => true,
+                'pre_password_size' => 0
+            ]
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['application.pre_password_size']);
+    }
+}
