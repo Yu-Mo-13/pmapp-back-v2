c9cfbd660b20089ca2c629d3d261d5735ddb07c5
Yuya Motozono <motozono.tech@gmail.com>
2026-01-25 18:30:54 +0900

    feat: アカウント作成API

--- routes/api/v2/account.php
+++ routes/api/v2/account.php
@@ -1,11 +1,17 @@
 <?php
 
-// アプリケーション管理
+// アカウント管理
 use App\Http\Controllers\Account\AccountIndexController;
+use App\Http\Controllers\Account\AccountCreateController;
+use App\Http\Controllers\Account\AccountApplicationIndexController;
 
 Route::prefix('/accounts')->group(function () {
     Route::middleware('auth:api')->group(function () {
         Route::get('/', AccountIndexController::class)
-            ->name('accounts.index');
+            ->name('accounts');
+        Route::get('/applications', AccountApplicationIndexController::class)
+            ->name('accounts.applications');
+        Route::post('/', AccountCreateController::class)
+            ->name('accounts');
     });
 });
