5c2c5ffbef05683634fa38b6aebcf57d4ec4883e
Yuya Motozono <motozono.tech@gmail.com>
2026-02-15 10:46:41 +0900

    feat: applicationのrouteにauthのmiddlewareを付加

--- routes/api/v2/application.php
+++ routes/api/v2/application.php
@@ -9,14 +9,16 @@ use App\Http\Controllers\Application\ApplicationUpdateController;
 use App\Http\Controllers\Application\ApplicationDeleteController;
 
 Route::prefix('/applications')->group(function () {
-    Route::get('/', ApplicationIndexController::class)
-        ->name('applications.index');
-    Route::post('/', ApplicationCreateController::class)
-        ->name('applications.create');
-    Route::get('/{application}', ApplicationShowController::class)
-        ->name('applications.show');
-    Route::put('/{application}', ApplicationUpdateController::class)
-        ->name('applications.update');
-    Route::delete('/{application}', ApplicationDeleteController::class)
-        ->name('applications.delete');
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/', ApplicationIndexController::class)
+            ->name('applications.index');
+        Route::post('/', ApplicationCreateController::class)
+            ->name('applications.create');
+        Route::get('/{application}', ApplicationShowController::class)
+            ->name('applications.show');
+        Route::put('/{application}', ApplicationUpdateController::class)
+            ->name('applications.update');
+        Route::delete('/{application}', ApplicationDeleteController::class)
+            ->name('applications.delete');
+        });
 });
